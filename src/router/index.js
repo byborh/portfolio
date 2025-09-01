5935893b94d679a0caa5d13fa3e8942e9f1df523
byborh <byborh@gmail.com>
2025-09-01 18:55:39 +0200

    wip - update datas

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory  } from 'vue-router'
+import { createRouter, createWebHistory  } from 'vue-router'
 import Home from '../components/Home.vue'
 import Contact from '../components/Contact.vue'
 
@@ -11,7 +11,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory (),
+  history: createWebHistory (),
   routes,
 })
 
